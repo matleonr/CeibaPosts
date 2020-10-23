0c1175f6bf121d7ff4fb4c97dbaba2f44a4a7bde
Matt Leon <mattleon@julians-MacBook-Pro.local>
2020-10-22 23:44:36 -0500

    xcode project creation

---
+++ CeibaPosts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CeibaPosts
+//
+//  Created by Matt Leon on 10/22/20.
+//  Copyright © 2020 Matt Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
