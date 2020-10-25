421be51cbd789e043b06d325a7cb8bdc19844444
Matt Leon <mattleon@julians-MacBook-Pro.local>
2020-10-25 16:30:57 -0500

    unit tests completed

--- CeibaPostsTests/CeibaPostsTests.swift
+++ CeibaPostsTests/CeibaPostsTests.swift
@@ -50,7 +50,7 @@ class fakeSuccessfullBL: CeibaBL {
     }
 }
 
-class UsersViewModelTests: XCTestCase {
+class CeibaPostsTests: XCTestCase {
     
     var scheduler: TestScheduler!
     var disposebag: DisposeBag!
@@ -77,6 +77,24 @@ class UsersViewModelTests: XCTestCase {
         XCTAssert(usersGetted![0] == fakeUsers[0])
     }
     
+    func testGetPosts()  {
+        let fakeBL = fakeSuccessfullBL()
+        let viewModel = UserPostsViewModel(ceibaBL: fakeBL)
+        
+        viewModel.input.viewShown.accept(true)
+        viewModel.input.userId.accept("1")
+        
+        let posts = scheduler.createObserver([Post]?.self)
+        viewModel.output.posts.asDriver().drive(posts).disposed(by: disposebag)
+        
+        scheduler.start()
+        
+        let postGetted = viewModel.output.posts.value
+        let fakePosts = createPosts()
+        
+        XCTAssert(postGetted![0] == fakePosts[0])
+    }
+    
     
     func createUsers() -> [User] {
         var users = [User]()
@@ -90,5 +108,26 @@ class UsersViewModelTests: XCTestCase {
         users.append(user3)
         
         return users
+    }
+    
+    func createPosts() -> [Post] {
+        var posts = [Post]()
+        
+        let post1 = Post(userId: 1, id: 1, title: "Post Decente", body: "por fin un post decente y no un lorem ipsum que no se entiende nada")
+        let post2 = Post(userId: 1, id: 2, title: "lorem ipsum :/", body: "no mentiras, este tampoco es lorem ipsum")
+        let post3 = Post(userId: 1, id: 3, title: "hola como estas?", body: "y tuve un impedimento con otra reunion que tenia atravezada, pero ya se acabo ya me estoy documentando sobre la inyeccion de dependencias")
+
+        let post4 = Post(userId: 2, id: 4, title: "mensaje de whatsapp", body: "Si por ahí se suscriben estaría genial voy a hacer un review de la guitarra ya bien con pros y contras en diciembre me compro la nueva")
+        let post5 = Post(userId: 2, id: 5, title: "ahora entiendo por que lorem ipsum", body: "um has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letras")
+        let post6 = Post(userId: 2, id: 6, title: " long established fact that a read", body: "d not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets cont")
+        
+        posts.append(post1)
+        posts.append(post2)
+        posts.append(post3)
+        posts.append(post4)
+        posts.append(post5)
+        posts.append(post6)
+        
+        return posts
     }
 }
