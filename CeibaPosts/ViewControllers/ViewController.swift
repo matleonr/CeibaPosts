8986e93af9ed8b57de144ba09de46cc7f89983a2
Matt León <mateo.vivo@gmail.com>
2020-10-24 21:22:10 -0500

    search bar functionality added

--- CeibaPosts/ViewControllers/ViewController.swift
+++ CeibaPosts/ViewControllers/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     let disposeBag = DisposeBag()
     let usersViewModel = UsersViewModel()
     var users: [User] = []
+    var filteredUsers = [User]()
+    var searching = false
 
     @IBOutlet weak var userSearchBar: UISearchBar!
     @IBOutlet weak var usersTableView: UITableView!
@@ -21,9 +23,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         title = "Users"
         bind()
-        
     }
-    
+        
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         usersViewModel.input.viewShown.accept(true)
@@ -31,7 +32,10 @@ class ViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        searching = false
         usersViewModel.input.viewShown.accept(false)
+        userSearchBar.searchTextField.text = ""
+        userSearchBar.endEditing(true)
         
     }
     
@@ -76,19 +80,53 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        users.count
+        
+        if searching{
+            return filteredUsers.count
+        }else{
+            return users.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let user = users[indexPath.row]
+        var user = User()
+        if searching{
+            user = filteredUsers[indexPath.row]
+        }else{
+            user = users[indexPath.row]
+        }
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as! UserCell
         cell.setCell(user: user)
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let user = users[indexPath.row]
+        var user = User()
+        if searching {
+            user = filteredUsers[indexPath.row]
+        }else{
+            user = users[indexPath.row]
+        }
+            
+
         performSegue(withIdentifier: "UserPosts", sender: user)
+        
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            searching = false
+            usersTableView.reloadData()
+        }else{
+            filteredUsers = users.filter({ (user) -> Bool in
+                return (user.name?.contains(searchText))!
+            })
+            searching = true
+            usersTableView.reloadData()
+        }
         
     }
 }
